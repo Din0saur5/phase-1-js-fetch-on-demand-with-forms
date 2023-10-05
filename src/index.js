d8bb1e29a0cc17f512e344c0a88c0f2ef2190386
Dan Hackworth <danielhackworth515@gmail.com>
2023-10-05 19:57:28 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
+    const form = document.querySelector("form");
+    form.addEventListener("submit", (e) =>{
+        e.preventDefault();
+        let movie = e.target.children[1]
+        // event 'targets' the form target.children => children of the form
+       
+
+    fetch("http://localhost:3000/movies")
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((data)=>{
+        const title = document.querySelector("section#movieDetails h4");
+        const desc = document.querySelector("section#movieDetails p");
+        
+    })
+    
+})
 }
 
 document.addEventListener('DOMContentLoaded', init);
